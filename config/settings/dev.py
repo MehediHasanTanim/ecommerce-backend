de7573295270a64483c04d83b0afff709ce6099c
Tanim Kabir <tkabir@jazzware.com>
2026-05-17 20:44:39 +0600

    fix(cors): explicitly allow frontend origin and credentials in dev mode

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -4,4 +4,10 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5177",
+    "http://127.0.0.1:5177",
+]
+CORS_ALLOW_CREDENTIALS = True
+
 # Debug-specific tools can be added here
